a89951ffd078209bcfca0bea98aa39645ae47bab
PranavXVis <vpranav2210@gmail.com>
2019-11-19 16:30:40 +0000

    My First Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Pranav Viswanathan, Period: 1]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Viswanathan_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Viswanathan_Projects.html">Projects.html</a></li></br>
+      <li><a href="Viswanathan_Resume.html">Resume.html</a></li></br>
+      <li><a href="Viswanathan_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
